eefbe7bbb72497beaaf23fa098207cfb855855a8
Małgorzata <mal.jakubik@gmail.com>
2023-12-30 20:48:56 +0100

    BatchNormalization and additional metrics added

--- LSTM/model.py
+++ LSTM/model.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential, load_model
 import tensorflow as tf
-from keras.layers import LSTM, Dense, Flatten
+from keras.layers import LSTM, Dense, Flatten, BatchNormalization
+import keras.metrics
 
 # num_companies = 1
 # lookback = 50 #How many days of the past can the model see
@@ -20,11 +21,15 @@ def build_model(num_features, lookback, horizon, learning_rate, dropout):
     model.add(LSTM(num_neurons_L2, activation = 'relu', dropout=dropout, return_sequences=False))
     model.add(Flatten())
     model.add(Dense(horizon, activation = 'sigmoid'))
+    model.add(BatchNormalization())
 
-    model.compile(loss='Huber', optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), metrics = ['mean_absolute_error'])
+    model.compile(loss='Huber', optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), metrics = ['mean_absolute_error',
+                                                                                                            'mse',
+                                                                                                            keras.metrics.RootMeanSquaredError(),
+                                                                                                            keras.metrics.MeanAbsolutePercentageError(),
+                                                                                                            keras.metrics.MeanSquaredLogarithmicError(),
+                                                                                                            keras.metrics.LogCoshError()
+                                                                                                            ])
 
     return model
 
-
-def load_model():
-    pass
